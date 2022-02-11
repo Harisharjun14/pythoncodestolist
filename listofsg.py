99e02e8aa9edc4498a2380742f15df9aa1546caa
Harisharjun14 <harisharjun14@gmail.com>
2022-02-11 11:15:34 +0530

    added files

---
+++ listofsg.py
@@ -0,0 +1,13 @@
+import boto3
+
+AWS_REGION = "ap-south-1"
+EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
+a=[]
+security_groups = EC2_RESOURCE.security_groups.all()
+
+print('Security Groups:')
+for security_group in security_groups:
+    print(f'  - Security Group {security_group.id}')
+    a.append(security_group.id)
+
+print(len(a))
